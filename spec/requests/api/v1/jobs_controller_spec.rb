01f19c896d49acfaa6976ac342eb8fe48d1fed93
helmiatwork <helmi.workspace@gmail.com>
2025-04-16 14:20:07 +0700

    add job controller spec

---
+++ spec/requests/api/v1/jobs_controller_spec.rb
@@ -0,0 +1,187 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::JobsController, type: :request do
+  let!(:user) { create(:user) }
+  let!(:job) { create(:job, user: user) }
+
+  def json
+    JSON.parse(response.body)
+  end
+
+  # API Request Tests
+  context "API Request" do
+    describe 'GET /api/v1/jobs' do
+      context "when Elasticsearch is up" do
+        before do
+          # Mock Elasticsearch Job.search
+          allow(Job).to receive(:search).and_return(double('result', results: double('results', records: [job])))
+        end
+
+        it 'returns a list of jobs and does not hit the database' do
+          get '/api/v1/jobs'
+
+          # Test the response
+          expect(response).to have_http_status(:ok)
+          expect(json).to be_an(Array)
+          expect(json.first["id"]).to eq(job.id)
+        end
+      end
+
+      context "when Elasticsearch is down" do
+        before do
+          # Simulate an Elasticsearch failure
+          allow(Job).to receive(:search).and_raise(StandardError.new("Elasticsearch is down"))
+        end
+
+        it "falls back to fetching jobs from the database" do
+          get '/api/v1/jobs'
+
+          expect(response).to have_http_status(:ok)
+          expect(json).to be_an(Array)
+          expect(json.first["id"]).to eq(job.id)
+        end
+      end
+    end
+
+    describe 'GET /api/v1/jobs/:id' do
+      it 'returns the job' do
+        get "/api/v1/jobs/#{job.id}"
+        expect(response).to have_http_status(:ok)
+        expect(JSON.parse(response.body)['id']).to eq(job.id)
+      end
+    end
+
+    describe 'POST /api/v1/jobs' do
+      context "given valid params" do
+        let(:valid_params) {
+          {
+            job: {
+              title: "New Job",
+              description: "Exciting work",
+              status: "pending",
+              user_id: user.id
+            }
+          }
+        }
+
+        it 'creates a new job' do
+          post '/api/v1/jobs', params: valid_params
+          expect(response).to have_http_status(:created)
+          expect(JSON.parse(response.body)['title']).to eq("New Job")
+        end
+      end
+
+      context "given invalid params" do
+        let(:invalid_job_params) { { job: { title: '', description: '', status: 'invalid', user_id: nil } } }
+
+        it 'returns errors when job creation fails due to invalid parameters' do
+          post '/api/v1/jobs', params: invalid_job_params
+
+          expect(response).to have_http_status(:unprocessable_entity)
+          body = JSON.parse(response.body)
+          expect(body['errors']).to include("Title can't be blank")
+          expect(body['errors']).to include("Description can't be blank")
+          expect(body['errors']).to include("User must exist")
+        end
+      end
+    end
+
+    describe 'PUT /api/v1/jobs/:id' do
+      context "given valid params" do
+        it 'updates the job' do
+          put "/api/v1/jobs/#{job.id}", params: { job: { title: "Updated Job" } }
+          expect(response).to have_http_status(:ok)
+          expect(JSON.parse(response.body)['title']).to eq("Updated Job")
+        end
+      end
+
+      context "given invalid params" do
+        it 'returns errors when job update fails due to invalid parameters' do
+          put "/api/v1/jobs/#{job.id}", params: { job: { title: '', description: '', status: 'invalid', user_id: nil } }
+
+          expect(response).to have_http_status(:unprocessable_entity)
+          body = JSON.parse(response.body)
+          expect(body['errors']).to include("Title can't be blank")
+          expect(body['errors']).to include("Description can't be blank")
+          expect(body['errors']).to include("User must exist")
+        end
+      end
+    end
+
+    describe 'DELETE /api/v1/jobs/:id' do
+      it 'deletes the job' do
+        delete "/api/v1/jobs/#{job.id}"
+        expect(response).to have_http_status(:no_content)
+        expect(Job.exists?(job.id)).to be_falsey
+      end
+    end
+  end
+
+  context "Test for cache" do
+    describe 'GET /api/v1/jobs/:id' do
+      context 'when job is cached' do
+        before do
+          # Write the job to cache
+          write_cache(job)
+        end
+
+        it 'returns the job from the cache' do
+          expect(Job).not_to receive(:find)  # Ensure DB is not queried
+          get "/api/v1/jobs/#{job.id}"
+          expect(response).to have_http_status(:ok)
+          expect(JSON.parse(response.body)['id']).to eq(job.id)
+        end
+      end
+
+      context 'when job is not cached' do
+        it 'fetches job from DB and stores it in the cache' do
+          delete_cache(job)
+          expect(Rails.cache).to receive(:fetch).with(job.id.to_s, namespace: 'job', expires_in: 60.minutes).and_call_original
+          get "/api/v1/jobs/#{job.id}"
+          expect(response).to have_http_status(:ok)
+        end
+      end
+    end
+
+    describe 'POST /api/v1/jobs' do
+      let(:valid_params) {
+        {
+          job: {
+            title: "New Job",
+            description: "Exciting work",
+            status: "pending",
+            user_id: user.id
+          }
+        }
+      }
+
+      it 'creates a new job and caches it' do
+        post '/api/v1/jobs', params: valid_params
+        created_job = Job.find_by(title: "New Job")
+        expect(Rails.cache.read(created_job.id, namespace: 'job')).to be_present
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    describe 'PUT /api/v1/jobs/:id' do
+      it 'updates the job and updates the cache' do
+        put "/api/v1/jobs/#{job.id}", params: { job: { title: "Updated Job" } }
+        cached_job = Rails.cache.read(job.id, namespace: 'job')
+        expect(cached_job.title).to eq("Updated Job")
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    describe 'DELETE /api/v1/jobs/:id' do
+      before do
+        write_cache(job)
+      end
+
+      it 'removes the job from the cache' do
+        delete "/api/v1/jobs/#{job.id}"
+        expect(Rails.cache.read(job.id, namespace: 'job')).to be_nil
+        expect(response).to have_http_status(:no_content)
+      end
+    end
+  end
+end
