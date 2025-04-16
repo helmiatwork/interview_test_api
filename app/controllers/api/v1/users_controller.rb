ee8dc9b4546a216a7d8069a0aa45dae1827ae1db
helmiatwork <helmi.workspace@gmail.com>
2025-04-16 14:21:04 +0700

    refactor user to implement cache

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,15 @@ module Api
 
       # GET /api/v1/users
       def index
-        @users = User.all
+        begin
+          # Try to fetch users from Elasticsearch
+          @users = User.search(query: { match_all: {}}).results.records.map { |record| record._source }
+        rescue StandardError => e
+          # If Elasticsearch is down or any error occurs, fallback to fetching users from the database
+          Rails.logger.error("Elasticsearch error: #{e.message}")
+          @users = User.includes(:jobs).all
+        end
+
         render json: @users
       end
 
@@ -19,15 +27,22 @@ module Api
         @user = User.new(user_params)
 
         if @user.save
+          # Write the newly created user to the cache
+          write_cache(@user)
           render json: @user, status: :created
         else
           render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
         end
+      rescue ActiveRecord::RecordNotUnique
+        render json: { errors: ['Email has already been taken'] }, status: :unprocessable_entity
       end
 
       # PATCH/PUT /api/v1/users/1
       def update
         if @user.update(user_params)
+          # Automatically overwrite the old cache with the updated user data
+          write_cache(@user)
+
           render json: @user
         else
           render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
@@ -36,14 +51,24 @@ module Api
 
       # DELETE /api/v1/users/1
       def destroy
+        # Remove the user from the cache before it is destroyed
+        delete_cache(@user)
+
+        # Now delete the user from the database
         @user.destroy
+
+        # Respond with no content
         head :no_content
       end
 
       private
 
       def set_user
-        @user = User.find(params[:id])
+        # Fetch user from cache or database
+        @user = fetch_cache(params[:id], klass: User)
+
+        # If user not found, return 404
+        render json: { errors: ['User not found'] }, status: :not_found unless @user
       end
 
       def user_params
